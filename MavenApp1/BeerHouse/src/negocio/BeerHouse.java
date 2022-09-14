b6fe153071004f8171c344132d7e3a7a4c95e9e2
Nogren99 <nogren23@gmail.com>
2022-09-13 23:01:57 -0300

    UML update

--- MavenApp1/BeerHouse/src/negocio/BeerHouse.java
+++ MavenApp1/BeerHouse/src/negocio/BeerHouse.java
@@ -1,6 +1,19 @@
 package negocio;
 
+import java.util.ArrayList;
+
+import modelo.Mesa;
+import modelo.Mozo;
+import modelo.Operario;
+import modelo.Producto;
+
 public class BeerHouse {
+    
+    ArrayList<Mozo> mozos = new ArrayList<Mozo>();
+    ArrayList<Mesa> mesa = new ArrayList<Mesa>();
+    ArrayList<Operario> operario = new ArrayList<Operario>();
+    ArrayList<Producto> producto = new ArrayList<Producto>();
+    
     public BeerHouse() {
         super();
     }
