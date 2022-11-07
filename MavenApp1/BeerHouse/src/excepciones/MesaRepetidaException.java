2f3cd9c62dec00dc6be889cf1f15f46c587a2fe6
Tomtechno <uzquiano.tomas@gmail.com>
2022-11-06 21:17:32 -0300

    5.1 y 5.2 - Terminar. 6.1 Terminar

---
+++ MavenApp1/BeerHouse/src/excepciones/MesaRepetidaException.java
@@ -0,0 +1,8 @@
+package excepciones;
+
+public class MesaRepetidaException extends Exception {
+
+	public MesaRepetidaException(String mensajito) {
+        super(mensajito);
+        
+}
--- MavenApp1/BeerHouse/src/modelo/Mesa.java
+++ MavenApp1/BeerHouse/src/modelo/Mesa.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import excepciones.CantComensalesException;
+
 public class Mesa {
 	private int numero;
     private int comensales;
@@ -8,14 +10,37 @@ public class Mesa {
     private Comanda comanda;
     
     
-	public Mesa(int numero, int comensales, Mozo mozo, String estado) {
+	public Mesa(int numero, int comensales, Mozo mozo, String estado) throws CantComensalesException {
 		super();
 		this.numero = numero;
 		this.comensales = comensales;
+		if(this.numero>1 && this.comensales<=1)
+			throw new CantComensalesException("La cantidad de comensales no es la indicada dado el nro. de mesa");
 		this.mozo = mozo;
 		this.estado = estado;
 	}
     
-	
+	public void AsignaMozoMesa(Mozo mozo) {
+		this.mozo = mozo;
+	}
+
+	public int getComensales() {
+		return comensales;
+	}
+
+	public Mozo getMozo() throws MesaSinMozoException{
+		if(this.mozo.equals(null))
+			throw new MesaSinMozoException("La mesa indicada no tiene mozo asociado");
+		else	
+			return mozo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public Comanda getComanda() {
+		return comanda;
+	}
     
 }
